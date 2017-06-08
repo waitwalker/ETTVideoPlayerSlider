8a64d5ac50be1ee0831cb36e7f3d417a17382973
WaitWalker <waitwalker@163.com>
2017-06-08 16:01:25 +0800

    初始化项目

---
+++ VideoPlayerSlider/VideoPlayerSlider/ETTVideoPlayerSlider/ETTVideoPlayerSlider.h
@@ -0,0 +1,13 @@
+//
+//  ETTVideoPlayerSlider.h
+//  VideoPlayerSlider
+//
+//  Created by LiuChuanan on 2017/6/8.
+//  Copyright © 2017年 waitWalker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ETTVideoPlayerSlider : UIView
+
+@end
---
+++ VideoPlayerSlider/VideoPlayerSlider/ETTVideoPlayerSlider/ETTVideoPlayerSlider.m
@@ -0,0 +1,21 @@
+//
+//  ETTVideoPlayerSlider.m
+//  VideoPlayerSlider
+//
+//  Created by LiuChuanan on 2017/6/8.
+//  Copyright © 2017年 waitWalker. All rights reserved.
+//
+
+#import "ETTVideoPlayerSlider.h"
+
+@implementation ETTVideoPlayerSlider
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
