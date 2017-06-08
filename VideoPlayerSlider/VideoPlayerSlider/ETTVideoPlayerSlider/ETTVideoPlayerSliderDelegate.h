0574ac6d42e3c8533b6e8eab084f30463bd08457
WaitWalker <waitwalker@163.com>
2017-06-08 16:03:54 +0800

    创建ETTVideoPlayerSliderDelegate

--- VideoPlayerSlider/VideoPlayerSlider/ETTVideoPlayerSlider/ETTVideoPlayerSliderDelegate.h
+++ VideoPlayerSlider/VideoPlayerSlider/ETTVideoPlayerSlider/ETTVideoPlayerSliderDelegate.h
@@ -1,14 +1,13 @@
 //
-//  ETTVideoPlayerSlider.h
+//  ETTVideoPlayerSliderDelegate.h
 //  VideoPlayerSlider
 //
 //  Created by LiuChuanan on 2017/6/8.
 //  Copyright © 2017年 waitWalker. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-@interface ETTVideoPlayerSlider : UIView
-
+@protocol ETTVideoPlayerSliderDelegate <NSObject>
 
 @end
