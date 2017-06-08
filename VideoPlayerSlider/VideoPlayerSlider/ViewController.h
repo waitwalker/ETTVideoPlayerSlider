84dc34a84d4c9c3a0401fd9cea1b3c407819b3a5
WaitWalker <waitwalker@163.com>
2017-06-08 15:55:33 +0800

    初始化项目

---
+++ VideoPlayerSlider/VideoPlayerSlider/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoPlayerSlider
+//
+//  Created by LiuChuanan on 2017/6/8.
+//  Copyright © 2017年 waitWalker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
